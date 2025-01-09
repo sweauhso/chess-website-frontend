95a82ce1549c2d5e506d2178acb1349c78d768b8
Joshua Geng <swegengjoshua@gmail.com>
2025-01-09 15:29:14 -0700

    Undid changes

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Enables React strict mode
-  basePath: "/chess-club-frontend", // GitHub Pages requires this
-  assetPrefix: "/chess-club-frontend/", // Ensures correct asset paths
-
-  images: {
-    unoptimized: true, // Disable Next.js image optimization for static hosting (GitHub Pages)
-  },
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@public": `${__dirname}/public`, // Allows easier imports from the public folder
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
